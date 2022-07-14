b249cb4509b1b07477b9521dbeeff2c1b98dc60b
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-07-14 11:27:50 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,33 @@
+// let button = document.getElementById("btn");
+//let buttons = document.querySelector("button");
+
+
+// button.addEventListener("click", (e)=>{
+
+//        // console.log(alert("Hi There")); 
+      
+// });
+// buttons.body.addEventListener("click", event => {
+//     if (event.target.nodeName == "BUTTON") {
+//       console.log("Clicked", event.target.textContent);
+//     }
+//   });
+
+// buttons.addEventListener("click", (e)=>{
+//     if(e.target.nodename== "BUTTON"){
+//         console.log("Clicked, e.target.textContent");
+//     }
+// });
+
+// window.addEventListener("keydown", event => {
+//     if (event.key == "v") {
+//       document.body.style.background = "violet";
+//     }
+//   });
+//   window.addEventListener("keyup", event => {
+//     if (event.key == "v") {
+//       document.body.style.background = "";
+//     }
+//   });
+
+v
